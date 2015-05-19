3ce0d5c4e2469cdc076efd4d0220f05e75ca4773
Adam Braus <ajbraus@gmail.com>
2015-05-19 10:11:48 -0700

    fixing

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 angular.module('starter', [])
+  
+  .controller("NavbarCtrl", ['$scope', '$rootScope', function ($scope, $rootScope) {
 
-  .controller("MyFirstCtrl", ['$scope', function ($scope) {
+    $scope.current_user = { points_count: 3 };
+
+    $rootScope.$on('addPoint', function() {
+      $scope.current_user.points_count = ++$scope.current_user.points_count
+    })
+  }])
+
+  .controller("MyFirstCtrl", ['$scope', '$rootScope', function ($scope, $rootScope) {
 
     $scope.foo = { bar: 'My First Angular App' }
-    
+
+    $scope.addPoint = function() {
+      $rootScope.$broadcast('addPoint')
+    }
   }]);
